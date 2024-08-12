48b0f3e25d0b0adb3c5fdf8437c07814a7be5be7
Matheus Da Mata <matheusdamatag@gmail.com>
2024-08-12 13:19:07 -0300

    fix: test validation and value-boject email structure

--- src/core/domain/entities/value-objects/email.spec.ts
+++ src/core/domain/entities/value-objects/email.spec.ts
@@ -3,11 +3,10 @@ import { Email } from "./email"
 describe('Email (Value-objects)', () => {
   it('should be able to create a valid email', () => {
     const email = Email.create('matheusdamatag@gmail.com')
-    expect(email.isValid).toEqual(true)
+    expect(email).toEqual('matheusdamatag@gmail.com')
   })
 
   it('should not be able to create an invalid email', () => {
-    const email = Email.create('matheusdamatag.com.br')
-    expect(email.isValid).toEqual(false)
+    expect(() => Email.create('matheusdamatag.com.br')).toThrowError(Error)
   })
 })
--- src/core/domain/entities/value-objects/email.ts
+++ src/core/domain/entities/value-objects/email.ts
@@ -1,3 +1,7 @@
+import { z } from "zod"
+
+const emailValidation = z.string().email()
+
 export class Email {
   public value: string
 
@@ -5,12 +9,17 @@ export class Email {
     this.value = value
   }
 
-  static create(email: string) {
-    return new Email(email)
+  static create(value: string) {
+    const email = new Email(value)
+
+    if (!email.isValid) throw new Error('Invalid email.')
+
+    return email.value
   }
 
   get isValid(): boolean {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    return regex.test(this.value)
+    const { success } = emailValidation.safeParse(this.value)
+
+    return success
   }
 }
